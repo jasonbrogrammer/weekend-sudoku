6094b2a8a2de9075e36ea0f1f3f5a0d723342fa5
jasonbrogrammer <jason.n.chang@gmail.com>
2017-12-16 14:14:20 -0800

    Wrote a solver

---
+++ solver.js
@@ -0,0 +1,52 @@
+/**
+ * Assumptions 9x9
+ * board is not invalid to start
+ * @param  {Array.<Array.<number>>} board [description]
+ * @return {boolean}       [description]
+ */
+const solveHelper = (board, inc = true) => {
+
+    const values = Array.from({length: board.length}).map((x, i) => i + 1);
+    if (!inc) values.reverse();
+
+    /**
+     * @returns {Array.<Array>} // Array of 'tuples'
+     */
+    const getRow = (row, col) => Array.from({length: 9}).map((x, i) => [row, i]);
+    const getCol = (row, col) => Array.from({length: 9}).map((x, i) => [i, col]);
+    const getBox = (row, col) => {
+        // get top left corner
+        const startRow = 3 * Math.trunc(row / 3);
+        const startCol = 3 * Math.trunc(col / 3);
+        const result = [];
+        for(let i = 0; i < 3; i++) {
+          for(let j = 0; j < 3; j++) {
+            result.push([startRow + i, startCol + j]);
+          }
+        }
+        return result;
+    };
+
+    const canPlace = (row, col, value) => [ getBox, getRow, getCol ]
+        .map(fn => fn(row, col))
+        .reduce((a, b) => a.concat(b))
+        .every(([row, col]) => board[row][col] !== value);
+
+
+    for (let i =0 ; i< 9*9; i++) {
+      const row = Math.trunc(i / 9);
+      const col = i % 9;
+      if (board[row][col]) continue;
+      for (const value of values) {
+        if (!canPlace(row, col, value)) continue;
+        board[row][col] = value;
+        if (solveHelper(board, inc)) return true;
+        board[row][col] = 0;
+      }
+      return false;
+    }
+
+    return true;
+};
+
+module.exports = { solveHelper };
